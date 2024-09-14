66cf46215022572a883a99170755056a3c15f7b4
apathriel <gabrielandersen1@gmail.com>
2024-09-14 13:08:55 +0200

    Add functionality for manipulating radius through input.
    Add two-way binding.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import RadialMenu from './components/radialMenu.vue';
+import UIModifier from './components/menuUIModifier.vue';
 
 const menuVisible = ref(false);
 const numMenuItems = ref(12);
 const cursorX = ref(0);
 const cursorY = ref(0);
+const menuRadius = ref(200);
 
 const handleClick = (event) => {
   menuVisible.value = !menuVisible.value;
@@ -20,11 +22,13 @@ onMounted(() => {
 
 <template>
   <h1>This is a radial menu prototype</h1>
+  <UIModifier v-model:radius="menuRadius"/>
   <RadialMenu
     :numItems="numMenuItems"
     :visible="menuVisible"
     :menuPosX="cursorX"
     :menuPosY="cursorY"
+    :radius="menuRadius"
   />
 </template>
 
--- src/components/menuUIModifier.vue
+++ src/components/menuUIModifier.vue
@@ -1,5 +1,54 @@
-<script setup></script>
+<script setup>
 
-<template></template>
+import { ref, watch } from 'vue';
 
-<style scoped></style>
+const props = defineProps({
+    radius: {type: Number, default: 200}
+})
+
+const emit = defineEmits(['update:radius'])
+
+const sliderValue = ref(props.radius)
+
+const handleClick = (e) => {
+    e.stopPropagation()
+}
+
+const updateRadius = () => {
+    emit('update:radius', Number(sliderValue.value))
+}
+
+// Watch for changes from the parent component, if it should pass any. 
+watch(() => props.radius, (newValue) => {
+  if (sliderValue.value !== newValue) {
+    sliderValue.value = newValue;
+  }
+});
+
+// Call updateRadius each time sliderValue is updated. Event is emitted with new (current) value.
+watch(sliderValue, (newValue) => {
+  updateRadius();
+});
+
+</script>
+
+<template>
+<div @click="handleClick" class="ui-modifier-menu-container">
+    <input v-model="sliderValue" type="range" min="100" max="500" class="inputSlider">
+    <input v-model="sliderValue" type="number" min="100" max="500">
+</div>
+
+</template>
+
+<style scoped>
+.ui-modifier-menu-container {
+    border-radius: 1rem;
+    padding: 1rem;
+    min-width: 20vw;
+    position: fixed;
+    top: 1rem;
+    right: 1rem;
+    border: 3px solid red;
+}
+
+</style>
